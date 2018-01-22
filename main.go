364732a847879f70ae0222c4c369998380a1c837
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2018-01-21 23:23:18 -0500

    getting up to date with substance package path change

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 	"github.com/ahmedalhulaibi/go-graphqlator-cli/cmd"
 
-	_ "github.com/ahmedalhulaibi/substance/dialects/mysqlsubstance"
-	_ "github.com/ahmedalhulaibi/substance/dialects/pgsqlsubstance"
+	_ "github.com/ahmedalhulaibi/substance/providers/mysqlsubstance"
+	_ "github.com/ahmedalhulaibi/substance/providers/pgsqlsubstance"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 )
